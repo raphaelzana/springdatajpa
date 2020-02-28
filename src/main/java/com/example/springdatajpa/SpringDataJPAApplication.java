6357ec2fe8459f0ac8ebf995f7c1d271b7b0a5f9
Junqueira Zanarelli <rjunqueiraza@dxc.com>
2020-02-28 17:20:51 -0300

    Added comment

--- src/main/java/com/example/springdatajpa/SpringDataJPAApplication.java
+++ src/main/java/com/example/springdatajpa/SpringDataJPAApplication.java
@@ -4,6 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Comment
+
 @SpringBootApplication
 @Slf4j
 public class SpringDataJPAApplication {
